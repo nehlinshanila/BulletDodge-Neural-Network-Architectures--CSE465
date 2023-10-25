1800e5b4fa8a28ca83028dcf3296c863f996ca21
nehlinshanila <nehlinshanila@gmail.com>
2023-10-25 22:39:35 +0600

    tried testing raycasting instead of fov overlap

--- Agents/fov_points.py
+++ Agents/fov_points.py
@@ -1,34 +1,47 @@
 import numpy as np
+import time
+import math
 
-from Constants.constants import SCREEN_HEIGHT, SCREEN_WIDTH, FOV_RADIUS
+from Constants.constants import SCREEN_HEIGHT, SCREEN_WIDTH, FOV_RADIUS, PREDATOR_RADIUS
 
 
-# ! time complexity of O(n)
 def get_fov_points(agent_position):
     fov_points = {}
     x, y = agent_position
-    # Loop that iterates from 0 to 359, representing all possible angles in degrees.
     for angle in range(360):
-        # Convert angle to radians
-        radians = np.deg2rad(angle)
-        # Calculate new coordinates within FOV based on polar coordinates 
-        x_coord = int(x + FOV_RADIUS * np.cos(radians))
-        y_coord = int(y + FOV_RADIUS * np.sin(radians))
-        # Check if the calculated x_coord and y_coord fall within the screen boundaries.
-        if 0 <= x_coord < SCREEN_WIDTH and 0 <= y_coord < SCREEN_HEIGHT:
-            # If yes, then add the coordinates to the fov_points list
-            fov_points[(x_coord, y_coord)] = 0
-            # fov_points.append((x_coord, y_coord))
-        else:
-            fov_points[(x_coord, y_coord)] = 1
+        radians = math.radians(angle)
+        for r in range(PREDATOR_RADIUS + 1, FOV_RADIUS + 1):
+            x_coord = int(x + r * math.cos(radians))
+            y_coord = int(y + r * math.sin(radians))
+            if 0 <= x_coord < SCREEN_WIDTH and 0 <= y_coord < SCREEN_HEIGHT:
+                fov_points[(x_coord, y_coord)] = 0  # Inside FOV
+            else:
+                fov_points[(x_coord, y_coord)] = 1  # Outside FOV (outside screen)
     return fov_points
 
 
+
+# run time
+if __name__ == '__main__':
+    # agent_pos = [50, 300]
+    # count = 0
+    # fov = get_fov_points(agent_pos)
+    # print(len(fov))
+    # for key, value in fov.items():
+    #     if key[0] < 0:
+    #         print(key[0], key[1])
+    # print(count)
+    agent_position = (250, 20)
+    start_time = time.time()
+    result = get_fov_points(agent_position)
+    print(len(result))
+    end_time = time.time()
+
+    elapsed_time = end_time - start_time
+    print(f"Time taken: {elapsed_time} seconds")
+
 # if __name__ == '__main__':
-#     agent_pos = [50, 300]
-#     count = 0
-#     fov = get_fov_points(agent_pos)
-#     for key, value in fov.items():
-#         if key[0] < 0:
-#             print(key[0], key[1])
-#     print(count)
+#     agent_location = [328.8917, 301.3598]
+#     print(get_fov_points(agent_location))
+
+
